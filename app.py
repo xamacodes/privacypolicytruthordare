bec5bfa14e7bddc89c87342702309ca5257f1be1
Sena Adansi <xamacodes@gmail.com>
2023-03-07 12:46:44 -0500

    Hi

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Privacy Policy", page_icon=":tada:", layout="wide")
+
+#HEADER
+
+st.subheader("This is the privacy policy for Truth or Dare!")
+st.title("Privacy Policy")
+st.write("We are committed to the privacy of our users. We do not collect or store any personal information.")
